8f5e77727557f3ef4be5da808219d9b1874b603e
lokiazdeyst <lokiazdeyst@gmail.com>
2020-04-19 16:40:28 -0500

    setup to run venv

--- pawtracker/__init__.py
+++ pawtracker/__init__.py
@@ -4,5 +4,5 @@ The flask application package.
 
 from flask import Flask
 app = Flask(__name__)
-
-import Paw_Tracker.views
+app.config['ENV'] = 'development'
+import pawtracker.views
--- pawtracker/views.py
+++ pawtracker/views.py
@@ -4,7 +4,7 @@ Routes and views for the flask application.
 
 from datetime import datetime
 from flask import render_template
-from Paw_Tracker import app
+from pawtracker import app
 
 @app.route('/')
 @app.route('/home')
